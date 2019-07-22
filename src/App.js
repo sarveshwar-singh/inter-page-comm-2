dfd14f2b5516c6f601846cae8953633e70b3483e
SARVESHWAR SINGH <sarveshwar@emurgo.io>
2019-07-22 19:10:11 +0900

    test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+window.addEventListener("message", receiveMessage, false);
+
+function receiveMessage(event) {
+  console.log(event.origin);
+  console.log(event.data);
+
+  if (event.origin !== "http://example.org:8080")
+    return;
+
+  // ...
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <span id="tSpan">ABDC</span>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -16,7 +29,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React 1
         </a>
       </header>
     </div>
